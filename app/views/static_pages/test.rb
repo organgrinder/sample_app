111b77d4999a1cf8eef704c3463babf25f5b92b2
Jay Harris <harris.jay@gmail.com>
2012-10-24 13:25:28 -0700

    attempted fix precompile error v8

--- app/views/static_pages/test.rb
+++ app/views/static_pages/test.rb
@@ -12,10 +12,15 @@ puts file_data
 =begin
   NEXT:
   1. make it work on Heroku
+  2. clean up the uneccesary stuff / comment
+  2. separate maps into a new github project
+  2. real url
+  2. style it with bootstrap CSS
   2. avoid loading file when not necessary
   3. display more information - zoom level, max/min elevation shown
   4. mouseover elevation?
   5. auto-redo heat map upon zoom or view change?
+  6. redo whole thing in openStreetMap to show elev of blocks/streets
   
   DONE:
   1. relative file path names
